70282bee20493528c0b446a18392509e99b62c3b
Luí s Fernando Guedes <lfernandoguedes@gmail.com>
2019-01-27 18:49:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board
-
+  print_row(true)
+  print_row(true)
+  print_row(false)
 end
 
 def print_row(line = false)
